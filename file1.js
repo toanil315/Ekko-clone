bed2718dc11978394e85cd2ff7bf7411e8496798
toan315 <congtoan.dn@gmail.com>
2020-05-02 23:44:42 +0700

    Ekko clone

---
+++ file1.js
@@ -0,0 +1,31 @@
+var demoMenuDisplay = document.getElementById("nav-menu-demo--display");
+var navMenuDemo = document.getElementById("nav-menu-demo");
+navMenuDemo.style.opacity = "0";
+demoMenuDisplay.onclick = function () {
+    if (navMenuDemo.style.opacity === "0") {
+        demoMenuDisplay.style.color = "var(--primary-color)";
+        navMenuDemo.style.opacity = "1";
+        navMenuDemo.style.visibility = "visible";
+
+    }
+    else {
+        demoMenuDisplay.style.color = "var(--text-color)";
+        navMenuDemo.style.opacity = "0";
+        navMenuDemo.style.visibility = "hidden";
+    }
+}
+var nav = document.getElementById("navbar");
+nav.style.boxShadow = "none";
+document.getElementById("wrapper").onscroll = function() {displayBoxShadow()};
+
+function displayBoxShadow() {
+    var y = document.getElementById("wrapper").scrollTop;
+    if(y !== 0) {
+        nav.style.boxShadow = "0 3px 10px rgba(0,0,0, 0.1)";
+    }
+    else {
+        nav.style.boxShadow = "none";
+    }
+}
+
+
